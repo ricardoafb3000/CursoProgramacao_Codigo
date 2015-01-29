02066566f52843b7eee9edea3c90a09ce60e468e
ricardo.barcellos <ricardoafb3000@gmail.com>
2015-01-29 19:57:43 -0200

    Inicial

---
+++ PessoasN/Validacao/ValidaBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PessoasN.Validacao
+{
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+    public class ValidaBase : System.Attribute
+    {
+
+        public string NomeUI { get; set; }
+        
+    }
+}
